ffe701bfe45f3213c21319de01934e37e54eeac9
nesbita <ariananesbit@gmail.com>
2021-07-11 15:00:54 -0600

    done

--- lab.js
+++ lab.js
@@ -17,12 +17,25 @@ console.log(realNumber);
 // ex: [10, '34', 3, '17']
 var mixedArray = [10, '34', 3, '17'];
 var bayOfIslands = ['Bay of Islands', 35.2167, 174.1541];
-// let suva: cityCoordinate = ['Suva' 18.1405° S, 178.4233° ];
-// let azores: string = ['37.7412° N, 25.6756° W'];
+var suva = ['Suva', 18.1405, 178.4233];
+var azores = ['Azores', 37.7412, 25.6756];
 // 🚨 uncomment this function to test your solution 🚨
 function printCityData(city) {
     console.log(city[0] + " is at Lat: " + city[1] + ", Long: " + city[2]);
 }
 printCityData(bayOfIslands);
-// Todo - Create union type here
+function area(s) {
+    if (s.kind === "square") {
+        return s.width * s.width;
+    }
+    else if (s.kind === "rectangle") {
+        return s.width * s.height;
+    }
+    else if (s.kind === "circle") {
+        return Math.PI * (Math.pow(s.radius, 2));
+    }
+    else {
+        return -1;
+    }
+}
 // Todo - write area function here
--- lab.ts
+++ lab.ts
@@ -29,18 +29,9 @@ let mixedArray: Array<string | number> = [10, '34', 3, '17']
 // Todo write the mixed array of strings and numbers
 
 // Todo write a function to convert the mixed array to an array of only numbers
-// function stringToNum(number: string[]) {
-//     forEach( (string: number) => {
-//         console.log(number)
-//     })
-// }
-
-// function stringToNumber = () => {
-//     mixedArray.forEach(string) => {
-//         console.log(number)
-//     }
-// }
-
+const stringToNum = (arr: (number | string)[]): number[] => {
+    return arr.map( element => Number(element))
+}
 
 /* --- Problem #3 - Tuple Type --- */
 // 1. Create a tuple type named 'cityCoordinate' that has three elements: city name, latitute, and longitude
@@ -50,8 +41,8 @@ let mixedArray: Array<string | number> = [10, '34', 3, '17']
 // Todo - create the type definition, and some cities
 type cityCoordinate = [string, number, number];
 let bayOfIslands: cityCoordinate = ['Bay of Islands', 35.2167, 174.1541];
-// let suva: cityCoordinate = ['Suva' 18.1405° S, 178.4233° ];
-// let azores: string = ['37.7412° N, 25.6756° W'];
+let suva: cityCoordinate = ['Suva', 18.1405, 178.4233];
+let azores: cityCoordinate = ['Azores', 37.7412, 25.6756]
 
 
 // 🚨 uncomment this function to test your solution 🚨
@@ -85,5 +76,20 @@ interface Circle {
 }
 
 // Todo - Create union type here
+type myShapes = Square | Rectangle | Circle
+
+function area(s: myShapes): number {
+    if (s.kind === "square") {
+        return s.width * s.width;
+    }
+    else if (s.kind === "rectangle") {
+        return s.width * s.height;
+    }
+    else if (s.kind === "circle") {
+        return Math.PI * (s.radius ** 2);
+    } else {
+        return -1;
+    }
+  }
 
 // Todo - write area function here
